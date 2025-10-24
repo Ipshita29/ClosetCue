16aa8cda783f95d61a546b6b2f0d4df8d98ecc5c
Kumar manak <manakkash20@gmail.com>
2025-10-24 21:51:01 +0530

    Add responsive Login page frontend with Autumn theme styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./components/login";
 import Contact from "./components/Contacts/contacts"; // Correct path to contacts.jsx
 
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
